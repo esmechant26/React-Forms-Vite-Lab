4280f23e5d06cd2352595f94540215478c855293
Esme Chant <esmemchant@gmail.com>
2026-04-23 08:30:59 -0400

    all tests pass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,14 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+  function handleAddItem(newItem) {
+    setItems((prevItems) => [...prevItems, newItem]);
   }
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+    <div>
+      <ShoppingList items={items} onItemFormSubmit={handleAddItem} />
     </div>
   );
 }
